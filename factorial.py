8000bec1972dc4033eb3445afbbe4233170a3db7
Javed167 <javed1320143@gmail.com>
2026-06-18 17:23:51 +0300

    add addition function

--- factorial.py
+++ factorial.py
@@ -3,6 +3,10 @@ num = int(input("Enter a number: "))
 factorial = 1
 
 for i in range(1, num + 1):
-    factorial *= i
+    factorial = factorial * i
 
 print("Factorial of", num, "is", factorial)
+number1 = int(input("Enter another number: "))
+number2 = int(input("Enter another number: "))
+result = number1 + number2
+print("The sum of", number1, "and", number2, "is", result)
